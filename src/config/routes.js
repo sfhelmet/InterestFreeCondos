aba55e10a19a084bfffeafad0ea21a3432135401
wam82 <monsterwam8282@gmail.com>
2024-02-27 17:07:42 -0500

    Public User Key Request Page

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,7 @@
 import Home from "../screens/Home";
 import Login from "../screens/Login";
 import NotFound from "../components/NotFound/NotFound";
+import PublicKeyRequest from "../components/PublicUserKeyRequest/PublicKeyRequest";
 
 const routes = [
   {
@@ -25,6 +26,12 @@ const routes = [
     path: "*",
     component: NotFound,
     name: "Not Found",
+    protected: false,
+  },
+  {
+    path: "/public-home",
+    component: PublicKeyRequest,
+    name: "Public Key Request",
     protected: false,
   },
 ];
