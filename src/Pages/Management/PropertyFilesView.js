9c8591cac83b42dfdba74736a2677b3a4ebed113
arcvelit <louka.f.s@gmail.com>
2024-03-21 20:20:51 -0400

    Fixed background color issue, updated styling to fit the websites style guidelines

--- src/Pages/Management/PropertyFilesView.js
+++ src/Pages/Management/PropertyFilesView.js
@@ -89,20 +89,21 @@ const PropertyFilesView = (props) =>
 
     return (
 
-    <div style={{"backgroundColor": "#193446"}}>
+<div style={{ backgroundColor: "#193446", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous"/>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
         <div className="container-fluid" style={{ width: "80%"}}>
+        <h1 style={{color: "#f4f9f9"}}>Property Files View</h1>
           {propertyProfiles ? (propertyProfiles.map( property => (
             <div className="row" key={property.id}>
             <div className="col">
             <div className="card m-2">
-              <h5 className="card-header">{property.propertyName}</h5>
-              <div className="card-body">
+              <h5 className="card-header" style={{color: "#7fe6ef",backgroundColor: "#213d4e"}}>{property.name}</h5>
+              <div className="card-body" style={{backgroundColor: "#3e8ca3"}}>
                 <div className="d-flex justify-content-between">
-                  <div className="propertyAddress">{property.fullAddress}</div>
+                  <div className="propertyAddress">{property.address}</div>
                   <button type="button" className="btn btn-primary" 
                           style={{ backgroundColor: "#317995", color: "#F4F9F9", border: "none"}}
                           onClick={() => handleUploadFileClick(property)}>
@@ -114,11 +115,11 @@ const PropertyFilesView = (props) =>
                   <p className="card-text">Files</p>
                   <ul className="fileList list-group">
                   {property.files && property.files.length > 0 ? (property.files.map( file =>
-                    <li className="list-group-item" key={`${file}_${property.id}`} >
+                    <li className="list-group-item" key={`${file}_${property.id}`} style={{backgroundColor: "#317995"}} >
                       <div className="d-flex justify-content-between">
                         <div>
                           <FileIcon filename={file}></FileIcon>
-                          <a id={file} href={`#${file}`} download>{file}</a>
+                          <a id={file} href={`#${file}`} download style={{color: "black"}}>{file}</a>
                         </div>
                         <button type="button" aria-label="Close" style={{border: "none", backgroundColor: "transparent"}}                                       
                                 onClick={() => handleDeleteConfirmation(file, property) }>
