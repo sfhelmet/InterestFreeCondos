b12553d60c3b1b618c96837f51c983cfedae59b5
arcvelit <louka.f.s@gmail.com>
2024-04-12 22:55:49 -0400

    Created page for request handling

---
+++ src/pages/Requests/Handling/RequestHandling.js
@@ -0,0 +1,19 @@
+import React, { useState} from 'react';
+import RequestService from '../RequestService' 
+import { auth } from "../../../config/firebase";
+import '../RequestStyling.css'
+
+
+const RequestHandling = () => {
+
+
+
+
+    return (
+        <div className='page-container'>
+        </div>
+    )
+}
+
+
+export default RequestHandling;
